abddfcafdcc8130b658d0ba68a8b71332fa5d75b
shilaranidas <shilarani.das@georgebrown.ca>
2020-12-21 00:27:00 -0500

    Add project files.

---
+++ FirstExample/02_IndexedQuads.cpp
@@ -0,0 +1,198 @@
+﻿
+///////////////////////////////////////////////////////////////////////
+//
+// 03_Projection.cpp - Just an introduction!
+//
+///////////////////////////////////////////////////////////////////////
+
+using namespace std;
+
+#include "stdlib.h"
+#include "time.h"
+#include "vgl.h"
+#include "LoadShaders.h"
+#include "glm\glm.hpp"
+#include "glm\gtc\matrix_transform.hpp"
+
+#define FPS 1
+#define X_AXIS glm::vec3(1,0,0)
+#define Y_AXIS glm::vec3(0,1,0)
+#define Z_AXIS glm::vec3(0,0,1)
+#define XY_AXIS glm::vec3(1,1,0)
+#define YZ_AXIS glm::vec3(0,1,1)
+#define XZ_AXIS glm::vec3(1,0,1)
+
+GLuint vao, ibo, points_vbo, colours_vbo, mvp_ID;
+glm::mat4 MVP, View, Projection;
+
+GLshort cube_indices[] = {
+	// Front.
+	0, 1, 2, 3,
+	// Left.
+	0, 4, 7, 3,
+	// Bottom.
+	0, 4, 5, 1,
+	// Right.
+	1, 5, 6, 2,
+	// Back.
+	6, 5, 4, 7,
+	// Top.
+	7, 6, 2, 3
+};
+
+GLfloat cube_vertices[] = {
+	-0.9f, -0.9f, 0.9f,		// 0.
+	0.9f, -0.9f, 0.9f,		// 1.
+	0.9f, 0.9f, 0.9f,		// 2.
+	-0.9f, 0.9f, 0.9f,		// 3.
+	-0.9f, -0.9f, -0.9f,	// 4.
+	0.9f, -0.9f, -0.9f,		// 5.
+	0.9f, 0.9f, -0.9f,		// 6.
+	-0.9f, 0.9f, -0.9f,		// 7.
+};
+
+GLfloat colours[] = {
+	1.0f, 0.0f, 0.0f,		// 0.
+	0.0f, 1.0f, 0.0f,		// 1.
+	0.0f, 0.0f, 1.0f,		// 2.
+	1.0f, 1.0f, 0.0f,		// 3.
+	1.0f, 0.0f, 1.0f,		// 4.
+	0.0f, 1.0f, 1.0f,		// 5.
+	0.5f, 0.0f, 0.0f,		// 6.
+	0.0f, 0.5f, 0.0f		// 7.
+};
+
+float scale = 0.5f, angle = 0.0f;
+
+void timer(int);
+
+void init(void)
+{
+	srand((unsigned)time(NULL));
+	//Specifying the name of vertex and fragment shaders.
+	ShaderInfo shaders[] = {
+		{ GL_VERTEX_SHADER, "triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+
+	//Loading and compiling shaders
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);	//My Pipeline is set up
+
+	mvp_ID = glGetUniformLocation(program, "MVP");
+
+	// Projection matrix : 45∞ Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	// Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	// Or, for an ortho camera :
+	Projection = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 100.0f); // In world coordinates
+
+	// Camera matrix
+	View = glm::lookAt(
+		glm::vec3(0, 0, 3.0f), // Camera is at (0,0,3), in World Space
+		glm::vec3(0, 0, 0),	 // and looks at the origin
+		glm::vec3(0, 1, 0)    // Head is up (set to 0,-1,0 to look upside-down)
+	);
+	
+	vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+		glGenBuffers(1, &ibo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cube_indices), cube_indices, GL_STATIC_DRAW);
+
+		points_vbo = 0;
+		glGenBuffers(1, &points_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(cube_vertices), cube_vertices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		glEnableVertexAttribArray(0);
+
+		colours_vbo = 0;
+		glGenBuffers(1, &colours_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, colours_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colours), colours, GL_STATIC_DRAW);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+		glEnableVertexAttribArray(1);
+		//glBindBuffer(GL_ARRAY_BUFFER, 0); // Can optionally unbind the buffer to avoid modification.
+
+	//glBindVertexArray(0); // Can optionally unbind the vertex array to avoid modification.
+
+	// Enable depth test and face culling.
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW);
+	glCullFace(GL_BACK);
+
+	timer(0); 
+}
+
+//---------------------------------------------------------------------
+//
+// transformModel
+//
+void transformObject(float scale, glm::vec3 rotationAxis, float rotationAngle, glm::vec3 translation) {
+	glm::mat4 Model;
+	Model = glm::mat4(1.0f);
+	Model = glm::translate(Model, translation);
+	Model = glm::rotate(Model, glm::radians(rotationAngle), rotationAxis);
+	Model = glm::scale(Model, glm::vec3(scale));
+	MVP = Projection * View * Model;
+	glUniformMatrix4fv(mvp_ID, 1, GL_FALSE, &MVP[0][0]);
+}
+
+//---------------------------------------------------------------------
+//
+// display
+//
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glBindVertexArray(vao);
+	
+	transformObject(scale, XY_AXIS, angle+=5.0f, glm::vec3(0.0f, 0.0f, 0.0f));
+
+	//Ordering the GPU to start the pipeline
+	glDrawElements(GL_LINE_LOOP, 24, GL_UNSIGNED_SHORT, 0);
+
+	glBindVertexArray(0); // Can optionally unbind the vertex array to avoid modification.
+
+	glutSwapBuffers(); // Now for a potentially smoother render.
+}
+
+//void idle()
+//{
+//	glutPostRedisplay(); // Tells glut to redisplay the current window.
+//}
+
+void timer(int) {
+	glutPostRedisplay();
+	glutTimerFunc(1000/FPS, timer, 0); // 60 FPS or 16.67ms.
+}
+
+//---------------------------------------------------------------------
+//
+// main
+//
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(1024, 1024);
+	glutCreateWindow("GAME2012 - Week 3");
+
+	glewInit();	//Initializes the glew and prepares the drawing pipeline.
+
+	init();
+
+	glutDisplayFunc(display);
+
+	//glutIdleFunc(idle); // We cannot use this to set the framerate, but we can set a callback to run when the window receives no events.
+
+	glutMainLoop();
+
+	return 0;
+}
